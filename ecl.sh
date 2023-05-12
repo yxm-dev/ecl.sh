eadd45fc8b91dded8d59f98c8b96f57d0d742417
yxm <yxm.dev@gmx.ie>
2023-05-12 17:33:50 -0300

    ...

---
+++ ecl.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+installdir="$HOME/.config/ecl.sh"
+
+source $installdir/files/nav
+source $installdir/files/cp
+source $installdir/files/rn
+source $installdir/files/mk
+source $installdir/files/del
+source $installdir/files/bkl
+source $installdir/files/vl
+source $installdir/files/wf
